56bd7f691e074af8cc4b0b40427e014c883a8181
ajmal250 <60932180+ajmal250@users.noreply.github.com>
2020-11-09 14:12:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask,render_template,request
+import matplotlib
+from flask import Flask, render_template, request, session, redirect, url_for
 from flaskext.mysql import MySQL
+from jedi.api.refactoring import inline
 from textblob import TextBlob
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from string import punctuation
+from collections import Counter
+
 
 app = Flask(__name__)
 
 
 mysql = MySQL()
+app.secret_key = 'your secret key'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'movie'
@@ -13,42 +22,144 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 
-#@app.route("/")
-#def main():
-#    return render_template('index.html')
-
-#@app.route("/",methods='POST')
-#def score():
- #   request.args.get('text')
- #   string=request.form['text']
-  #  processed_text = string.upper()
-   # return processed_text
-    #string='very good'
-    #analysis = TextBlob(string).sentiment.polarity
-    #return render_template('index.html',mydata=analysis)
-
-
-@app.route('/')
-def my_form():
+@app.route("/")
+def main():
     return render_template('index.html')
 
 
-@app.route('/', methods=['POST'])
-def my_form_post():
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/signup', methods=['POST'])
+def signuppost():
+    name = request.form['uname']
+    password=request.form['password']
+    cpass = request.form['cpassword']
+    if name =='':
+        exe='enter user name!!!'
+    elif password =='':
+        exe='enter password!!!'
+    elif password == cpass and name !='' and password!='':
+        conn=mysql.connect()
+        cur=conn.cursor()
+        #cur.execute('''SELECT MAX(userid) FROM user''')
+        #maxid=cur.fetchone()
+        cur.execute('''INSERT INTO user (username,password) VALUES (%s,%s)''',(name,password))
+        cur.close()
+        exe='user : '+name+' created'
+    else:
+        exe='incorrect password!!!'
+    return render_template('signup.html',data=exe)
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+
+@app.route("/movie")
+def movie():
+    return render_template('movie.html')
+
+
+@app.route('/rating')
+def rating():
+    return render_template('rating.html')
+
+
+@app.route('/rating', methods=['POST'])
+def ratingpost():
     text = request.form['text']
     processed_text = TextBlob(text).sentiment.polarity
-    return render_template('index.html',mydata=processed_text)
+    return render_template('rating.html', mydata=processed_text)
+
+
+@app.route('/userrating')
+def userrating():
+    return render_template('userrating.html')
+
+
+@app.route('/usermovie')
+def usermovie():
+    return render_template('usermovie.html')
+
+
+@app.route('/userrating',methods=['POST'])
+def userratingpost():
+    text = request.form['text']
+    processed_text = TextBlob(text).sentiment.polarity
+    return render_template('userrating.html', mydata=processed_text)
+
 
 
 @app.route("/login")
 def login():
-    conn = mysql.connect()
-    cursor =conn.cursor()
+    #conn = mysql.connect()
+    #cur =conn.cursor()
+    #cur.execute("SELECT * from users")
+    #result = cursor.fetchall()
+    #cursor.close()
+    return render_template('login.html')
 
-    cursor.execute("SELECT * from users")
-    result = cursor.fetchall()
-    cursor.close()
-    return render_template('login.html', mydata=result)
+
+@app.route('/login',methods=['POST'])
+def logincheck():
+    name=request.form['uname']
+    password=request.form['password']
+    msg=''
+    if name!='' and password !='':
+        conn=mysql.connect()
+        cur=conn.cursor()
+        cur.execute('''SELECT * FROM user WHERE username=%s AND password=%s''',(name,password))
+        account = cur.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account[0]
+            session['username'] = account[1]
+            return render_template('user.html')
+        else:
+            msg='Incorrect username/password!'
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+
+@app.route('/user')
+def userpage():
+    return render_template('user.html')
+
+
+@app.route("/about")
+def about():
+    id='2'
+    conn = mysql.connect()
+    #cur = conn.cursor()
+    #cur.execute('''SELECT comment FROM comment WHERE movieid=%s''', (movieid))
+    #data = cur.fetchall()
+    #df=pd.DataFrame(data, columns=cur.)
+    sql='''SELECT comment FROM comment WHERE movieid='''+id
+    df = pd.read_sql(sql=sql, con=conn)
+
+    return render_template('about.html',mydata=df)
+
+
+@app.route("/userabout")
+def usercontact():
+    return render_template('contactuser.html')
+
 
 
 if __name__ == "__main__":
